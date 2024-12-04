36cfbad4af8c9a96741c0a0858f32387666d08b6
Harish Bendale <120021183+harish-bendale@users.noreply.github.com>
2024-12-04 10:13:32 +0000

    Datatypes code added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as a newer version
+let isLoggedIn = false
+
+// number => 2 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "hitesh");
+console.log("Null type ",typeof null);
+console.log("Undefined type ",typeof undefined);
