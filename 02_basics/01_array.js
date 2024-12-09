c585cba04f16dd334f5d176036eab1a0b7a9ce21
harish-bendale <harishbendale80@gmail.com>
2024-12-09 18:59:17 +0530

    Array code added

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -16,8 +16,8 @@ const myArr2 = new Array(1,2,3,4)
 // console.log(myArr);
 
 
-myArr.unshift(9) // [9, 0, 1, 2,3, 4, 5]
-myArr.shift() // [0, 1, 2,3, 4, 5]
+myArr.unshift(9) // [9, 0, 1, 2, 3, 4, 5]
+myArr.shift() // [0, 1, 2, 3, 4, 5]
 
 console.log(myArr.includes(9)); // false
 console.log(myArr.indexOf(3)) // 3
